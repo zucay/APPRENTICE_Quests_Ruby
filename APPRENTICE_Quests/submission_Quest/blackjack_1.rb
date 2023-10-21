318b7871fdca70443207be68ea242323940b6a0b
Taku1103 <orzorz1243@gmail.com>
2023-10-22 01:52:54 +0900

    乱数ドロー変更前

--- APPRENTICE_Quests/submission_Quest/blackjack_1.rb
+++ APPRENTICE_Quests/submission_Quest/blackjack_1.rb
@@ -5,7 +5,7 @@
 # 日本語訳
 # 被りあり
 # 〇Dealerの手札隠す
-
+# 手札を追加機能も関数にまとめる
 
 # プレイヤークラス
 class Player
@@ -22,7 +22,12 @@ class Dealer
 end
 
 # 変数
-used_cards = []
+deck = []
+suits = ['Heart', 'Diamond', 'Club', 'Spade']
+nums = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+used_cards = [] # 使用したカード配列
+
+
 player_hand = []
 dealer_hand = []
 
@@ -52,10 +57,28 @@ def rand_card()
   return card
 end
 
-# 配る クラスだったら機能
-def distribution(player_name)
-  player_name.push rand_card()
+# デッキ生成
+def deck_making()
+  suits.each do |suit|
+    nums.each do |num|
+      deck << [num, suit]
+    end
+  end
 end
+
+
+# デッキからカード選択(引数：デッキ、使ったカードリスト)
+def select_card(deck, used_cards)
+  available_cards = deck - used_cards #配列の差分
+  selected_card = available_cards.sample
+  used_cards << selected_card
+  return selected_card
+end
+
+# # 配る クラス化したら機能出来そう
+# def distribution(player_name)
+#   player_name.push rand_card()
+# end
 
 # 実行部
 if __FILE__ == $PROGRAM_NAME
@@ -68,42 +91,44 @@ if __FILE__ == $PROGRAM_NAME
   dealer_hand.push rand_card()
   dealer_hand.push rand_card()
 
-# 手札、点数公開処理
-  puts "Your hand is #{player_hand}"
-  puts "Dealer hand is #{dealer_hand[0]}"
+  # 手札、点数公開処理
+  puts "あなたの手札は#{player_hand}です。"
+  puts "ディーラーの引いたカードは #{dealer_hand[0]}です。"
+  puts "ディーラーの引いた2枚目のカードはわかりません。"
 
   player_score = calculate(player_hand)
   # dealer_score = calculate(dealer_hand)
-  puts "Player's score is #{player_score}"
   # puts "dealer's score is #{dealer_score}"
 
 
 # main loop-----------------------------
   while true
-    puts "You wanna draw? (y/n)"
+    puts "あなたの現在の得点は#{player_score}です。カードを引きますか？(y/n)"
     y_or_n = gets.chomp
-
+    # 入力部
     case y_or_n
     when "n"
       break
     when "y"
       player_hand.push rand_card()
       player_score = calculate(player_hand)
-      p player_hand
-      puts "Player's score is #{player_score}"
 
+      # 早期ループ脱出
       if player_score > 21
         puts"BURST! You Lose!"
+        puts"ブラックジャックを終了します。"
         exit
       end
-
       if player_score == 21
         puts"You are BJ!"
         break
       end
+    else
+      puts "y/n を入力してください"
     end
   end
 
+  # ディーラードロー
   while dealer_score < 17
     rand_hand = rand_card()
     dealer_hand.push rand_hand
@@ -115,16 +140,20 @@ if __FILE__ == $PROGRAM_NAME
   puts "Player's score is #{player_score}"
   puts "Dealer's score is #{dealer_score}"
 
+  # 勝敗判定
   if ((21 - player_score) < (21 - dealer_score))
-    puts "YOU WIN!"
+    puts "あなたの勝ちです！"
+    puts "ブラックジャックを終了します。"
     exit
   elsif  22 <= dealer_score
-    puts "Dealer is Bursted! YOU WIN!"
+    puts "ディーラーのバーストです。あなたの勝ちです！"
   elsif (21 - player_score) == (21 - dealer_score)
-    puts "Draw!"
+    puts "引き分けです！"
+    puts "ブラックジャックを終了します。"
     exit
   else
-    puts "YOU LOSE!"
+    puts "あなたの負けです！"
+    puts "ブラックジャックを終了します。"
     exit
   end
 
