2c2c5fd0d7bcd102eadfcd094cc417fc795fb2fe
Taku1103 <orzorz1243@gmail.com>
2023-10-23 22:02:31 +0900

    BJ_2をクラス化

---
+++ APPRENTICE_Quests/submission_Quest/blackjack_2.rb
@@ -0,0 +1,197 @@
+# frozen_string_literal: true
+
+# 〇一回しか引けない end位置変更
+# 〇burstしても2重ループ抜けれない->exit?
+# 〇被りあり
+# 〇Dealerの手札隠す
+# 〇Aの得点場合分け
+
+# プレイヤークラス
+class Player
+  def initialize()
+    #手札
+    #点数
+    #バーストしたか
+  end
+
+end
+
+# ディーラークラス
+class Dealer
+
+end
+
+# デッキクラス
+
+# 変数
+used_cards = [] # 使用したカード配列
+
+player_hand = []
+dealer_hand = []
+
+player_score = 0
+dealer_score = 0
+
+is_player_bust = false
+is_dealer_bust = false
+
+# 得点計算関数(引数:2次元配列のみ)
+def calculate(hand_arr)
+  sum = 0
+  ace_count = 0
+  score_10 = ['10', 'J', 'Q', 'K']
+
+  hand_arr.each do |hand|
+    num_str = hand[0]
+    if score_10.include?(num_str)
+      num = 10
+    elsif num_str == 'A'
+      num = 11
+      ace_count += 1
+    else
+      num = num_str.to_i
+    end
+    sum += num
+  end
+
+  # Aの場合分け処理 sum > 21 ならAを1として計算
+  ace_count.times do
+    if sum > 21
+      sum -= 10
+    end
+  end
+
+  return sum
+end
+
+# デッキ生成
+def deck_making()
+  deck = []
+  suits = ['Heart', 'Diamond', 'Club', 'Spade']
+  nums = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+  suits.each do |suit|
+    nums.each do |num|
+      deck << [num, suit]
+    end
+  end
+  return deck
+end
+
+# デッキからカード選択(引数：デッキ、使ったカードリスト)
+def select_card(deck, used_cards)
+  available_cards = deck - used_cards # 配列の差分
+  selected_card = available_cards.sample # ランダム1枚
+  used_cards << selected_card #　使用したカードを記録
+  return selected_card
+end
+
+
+# 実行部---------------------------------
+if __FILE__ == $PROGRAM_NAME
+  puts "ブラックジャックを開始します。"
+
+  #デッキ生成
+  deck = deck_making()
+
+  #手札配り
+  player_hand << select_card(deck, used_cards)
+  player_hand << select_card(deck, used_cards)
+
+  dealer_hand << select_card(deck, used_cards)
+  dealer_hand << select_card(deck, used_cards)
+
+  # 手札、点数情報の公開
+  puts "あなたの手札は#{player_hand}です。"
+  puts "ディーラーの引いたカードは #{dealer_hand[0]}です。"
+  puts "ディーラーの引いた2枚目のカードはわかりません。"
+
+  player_score = calculate(player_hand)
+  dealer_score = calculate(dealer_hand)
+  # puts "dealer's score is #{dealer_score}"
+
+# ループ部
+  while true
+    puts "あなたの現在の得点は#{player_score}です。カードを引きますか？(y/n)"
+    y_or_n = gets.chomp
+    # 入力部
+    case y_or_n
+    when "n"
+      break
+    when "y"
+
+      player_draw_card = select_card(deck, used_cards)
+      player_hand << player_draw_card
+      player_score = calculate(player_hand)
+      puts"あなたの引いたカードは#{player_draw_card}です。"
+
+      # プレイヤー バースト or 21判定
+      if player_score > 21
+        is_player_bust = true
+        puts"あなたの現在の得点は#{player_score}です"
+        break
+      elsif player_score == 21
+        puts"You are BJ!"
+        puts"あなたの現在の得点は#{player_score}です"
+        break
+      end
+    else
+      puts "y/n を入力してください"
+    end
+  end
+
+  # ディーラードロー
+  while dealer_score < 17
+    dealer_draw_card  = select_card(deck, used_cards)
+    dealer_hand << dealer_draw_card
+    dealer_score = calculate(dealer_hand)
+    puts"ディーラーの引いたカードは#{dealer_draw_card}です。"
+    puts "Dealer's score is #{dealer_score}"
+  end
+
+  # ディーラーバースト判定
+  if 21 < dealer_score
+    is_dealer_bust = true
+  end
+
+  puts "ディーラーの手札は#{dealer_hand}です。"
+
+  puts "Player's score is #{player_score}"
+  puts "Dealer's score is #{dealer_score}"
+
+
+  # 勝敗判定
+
+  #バースト判定
+  if is_player_bust || is_dealer_bust
+    if is_player_bust && is_dealer_bust
+      puts "両者バーストです！"
+      puts "引き分けです！"
+      puts "ブラックジャックを終了します。"
+    elsif is_player_bust
+      puts "あなたのバーストです！"
+      puts "あなたの負けです！"
+      puts "ブラックジャックを終了します。"
+    elsif is_dealer_bust
+      puts "ディーラーのバーストです！"
+      puts "あなたの勝ちです！"
+      puts "ブラックジャックを終了します。"
+    end
+    exit
+  end
+
+  # 点数勝敗判定
+  diff_21_player_score = 21 - player_score
+  diff_21_dealer_score = 21 - dealer_score
+
+  if diff_21_player_score < diff_21_dealer_score
+    puts "あなたの勝ちです！"
+    puts "ブラックジャックを終了します。"
+  elsif diff_21_player_score == diff_21_dealer_score
+    puts "引き分けです！"
+    puts "ブラックジャックを終了します。"
+  elsif diff_21_player_score > diff_21_dealer_score
+    puts "あなたの負けです！"
+    puts "ブラックジャックを終了します。"
+  end
+  exit
+end
---
+++ APPRENTICE_Quests/submission_Quest/blackjack_2_obj.rb
@@ -0,0 +1,244 @@
+# frozen_string_literal: true
+
+# 〇一回しか引けない end位置変更
+# 〇burstしても2重ループ抜けれない->exit?
+# 〇被りあり
+# 〇Dealerの手札隠す
+# 〇Aの得点場合分け
+
+# クラス---------------------------------------------------------
+# BJ_playerクラス（親クラス）
+class BJ_player
+  attr_accessor :name, :hand, :score, :is_bust , :is_BJ
+
+  def initialize(name)
+    @name = name
+    @hand = []
+    @score = 0
+    @is_bust = false
+    @is_BJ = false
+  end
+
+  # 得点計算、バースト判定
+  def calculate()
+    sum = 0
+    ace_count = 0
+    score_10 = ['10', 'J', 'Q', 'K']
+
+    @hand.each do |hand|
+      num_str = hand[0]
+      if score_10.include?(num_str)
+        num = 10
+      elsif num_str == 'A'
+        num = 11
+        ace_count += 1
+      else
+        num = num_str.to_i
+      end
+      sum += num
+    end
+    # Aの場合分け処理 sum > 21 ならAを1として計算
+    ace_count.times do
+      if sum > 21
+        sum -= 10
+      end
+    end
+    @score = sum
+
+    #バースト、21判定
+    if @score > 21
+      @is_bust = true
+    elsif @score == 21
+      @is_BJ = true
+    end
+  end
+
+  # 手札公開メソッド
+  def show_hand()
+    puts "#{@name}の手札は#{@hand}です。"
+  end
+
+  # 点数公開メソッド
+  def show_score()
+    puts "#{@name}の現在の得点は#{@score}です。"
+  end
+
+  # ドロー公開メソッド
+  def show_latest_draw()
+    puts "#{@name}の引いたカードは#{@hand[-1]}です。"
+  end
+
+end
+
+# Playerクラス-----------------------------------------------------------------------
+class Player < BJ_player
+  #ドローメソッド
+  def player_draw(deck_obj , deck)
+    while true #"y"入力 or 21以上までループ
+      puts "カードを引きますか？(y/n)"
+      y_or_n = gets.chomp
+      case y_or_n
+      when "n"
+        break
+      when "y"
+        deck_obj.distribution(deck, self)
+        show_latest_draw()
+        calculate()
+        show_score()
+        if @is_BJ || @is_bust
+          break
+        end
+      else
+        puts "y/n を入力してください"
+      end
+    end
+  end
+end
+
+
+# Dealerクラス-----------------------------------------------------------------------
+class Dealer < BJ_player
+  # ドローメソッド（17以上までドロー）
+  def dealer_draw(deck_obj, deck)
+    show_score()
+    while @score < 17
+      deck_obj.distribution(deck, self)
+      calculate()
+      show_latest_draw()
+      show_score()
+    end
+  end
+
+  # 手札1枚公開
+  def show_one_hand
+    puts "#{@name}の引いたカードは #{@hand[0]}です。"
+    puts "#{@name}の引いた2枚目のカードはわかりません。"
+  end
+end
+
+# Deckクラス-----------------------------------------------------------------------
+class Deck
+  attr_accessor :used_cards
+
+  def initialize(name)
+    @name = name
+    @used_cards = []
+  end
+
+  # デッキ作成メソッド
+  def deck_making()
+    deck_arr = []
+    suits = ['Heart', 'Diamond', 'Club', 'Spade']
+    nums = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    suits.each do |suit|
+      nums.each do |num|
+        deck_arr << [num, suit]
+      end
+    end
+    return deck_arr
+  end
+
+  # デッキからランダムカード排出メソッド（被り無し）
+  def select_card(deck)
+    available_cards = deck - @used_cards   # 配列の差分
+    selected_card = available_cards.sample # ランダム1枚
+    @used_cards << selected_card           # 使用したカードを記録
+    return selected_card
+  end
+
+  # 配布メソッド
+  def distribution(deck, bj_player_obj)
+    bj_player_obj.hand << select_card(deck)
+  end
+end
+
+
+# Gameクラス-----------------------------------------------------------------------
+class Game
+  def initialize(name1, name2)
+    @in_game_player_name1 = name1
+    @in_game_player_name2 = name2
+  end
+
+  # 開始メソッド
+  def start()
+    puts"ブラックジャックを開始します。"
+  end
+
+  # 終了メソッド
+  def end()
+    puts"ブラックジャックを終了します。"
+    exit
+  end
+
+  # 勝敗判定メソッド
+  def win_or_lose(player_obj, dealer_obj)
+    # バースト勝敗判定
+    if player_obj.is_bust || dealer_obj.is_bust
+      if player_obj.is_bust && dealer_obj.is_bust
+        puts "両者バーストです！"
+        puts "引き分けです！"
+      elsif player_obj.is_bust
+        puts "あなたのバーストです！"
+        puts "あなたの負けです！"
+      elsif dealer_obj.is_bust
+        puts "ディーラーのバーストです！"
+        puts "あなたの勝ちです！"
+      end
+    #点数勝敗判定
+    else
+      if 21 - player_obj.score < 21 - dealer_obj.score
+        puts "あなたの勝ちです！"
+      elsif 21 - player_obj.score == 21 - dealer_obj.score
+        puts "引き分けです！"
+      elsif 21 - player_obj.score > 21 - dealer_obj.score
+        puts "あなたの負けです！"
+      end
+    end
+  end
+end
+
+# 実行部(メインファイルなら以下実行)-----------------------------------------------------------------------
+if __FILE__ == $PROGRAM_NAME
+  #インスタンス化
+  bj_game = Game.new("dealer", "player")
+  player = Player.new("player")
+  dealer = Dealer.new("dealer")
+  deck_obj = Deck.new("bj_deck")
+
+  # BJ開始
+  bj_game.start
+
+  # デッキ生成
+  bj_deck = deck_obj.deck_making
+
+  #手札配り(引数:デッキ,参加者のハンド)
+  deck_obj.distribution(bj_deck, player)
+  deck_obj.distribution(bj_deck, player)
+
+  deck_obj.distribution(bj_deck, dealer)
+  deck_obj.distribution(bj_deck, dealer)
+
+  # 点数計算
+  player.calculate
+  dealer.calculate
+
+  #点数とハンド表示
+  player.show_hand
+  dealer.show_one_hand
+  player.show_score
+  dealer.show_hand #debug用
+  dealer.show_score #debug用
+
+  # プレイヤードロー 入力ループ部
+  player.player_draw(deck_obj, bj_deck)
+
+  # ディーラードロー
+  dealer.dealer_draw(deck_obj, bj_deck)
+
+  # 勝敗判定
+  bj_game.win_or_lose(player, dealer)
+
+  # BJ終了
+  bj_game.end
+end
