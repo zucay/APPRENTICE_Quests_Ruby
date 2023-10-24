1b0336329ba2dad7c11e1efbc9183962c01e309d
Taku1103 <orzorz1243@gmail.com>
2023-10-25 01:37:26 +0900

    10/24(火) BJ_step3取り掛かり

---
+++ APPRENTICE_Quests/submission_Quest/objtest.rb
@@ -0,0 +1,22 @@
+class User
+
+end
+
+# user = User.new
+# puts user.to_s
+# puts user.nil?
+
+# puts User.superclass
+# # p user.methods
+# p user.instance_of?(User)
+# p user.instance_of?(String)
+
+# p "--------------"
+# p user.is_a?(Object)
+# p user.is_a?(User)
+# p user.is_a?(BasicObject)
+
+# p user.is_a?(String)
+# p BasicObject.superclass
+arr = [1,2,3,4,5]
+puts arr.join(" ")
